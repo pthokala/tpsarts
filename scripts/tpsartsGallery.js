3a624d5c848299b55f6afd472a7e92761849ef6b
pthokala <pardhu.thokala@gmail.com>
2016-07-05 19:34:29 -0400

    changed view mode to dark

--- scripts/tpsartsGallery.js
+++ scripts/tpsartsGallery.js
@@ -1,11 +1,23 @@
 /**
  * Created by PardhuThokala on 6/26/16.
  */
-function fnShowImageInModel(imgPath){
-    console.log(imgPath);
+function fnShowImageInModel(imgPath, isVertical){
     $('#img-showImage').attr('src', imgPath);
     
     $('#imageModal').modal({
         keyboard: false
-    })
+    });
+    if(isVertical){
+        var imgHeight = '900px';
+        $( "#img-showImage" ).height(imgHeight);
+        $( "#img-showImage" ).width('auto');
+    } else {
+        $( "#img-showImage" ).width('100%');
+        $( "#img-showImage" ).height('auto');
+    }
 }
+$(document).on("click", "#imageModal", function(event){
+    $('body').removeClass('modal-open');
+    $('.modal-backdrop').remove();
+    $('#imageModal').modal('hide');
+});
